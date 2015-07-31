8b0d5a333530b25382fd5fc945770472b4525b5f
Tsuboya Sho <tsuboya.sho@future.co.jp>
2015-07-31 12:44:51 +0900

    test

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('testApp')
     .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        var $uri ='data/sample.json';
+        var $uri ='https://api.typeform.com/v0/form/E9Tz2g?key=9558ca6453cfd4e8f49ceed4bca5b11aeab62cf0&completed=true';
 
         $scope.doSearch = function() {
 
@@ -11,7 +11,7 @@ angular.module('testApp')
                 method : 'GET',
                 url : $uri
             }).success(function(data, status, headers, config) {
-                $scope.results = data.data;
+                $scope.results = data.responses;
                 console.log(status);
                 console.log(data);
             }).error(function(data, status, headers, config) {
